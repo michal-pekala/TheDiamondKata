5c114d5edcc6b4d172eee55e6253a88c6eeec331
Michal Pekala <MP3X@sdc.dk>
2024-05-03 12:53:52 +0200

    program.cs + console output

--- TheDiamondKata/Program.cs
+++ TheDiamondKata/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using TheDiamondKata;
+
+if (args.Length != 1 || args[0].Length != 1)
+		Console.WriteLine("1 arg expected: letter A-Z");
+
+var size = char.Parse(args[0]) - 'A' + 1;
+
+var printer = new DiamondKata(size);
+
+for (int i = 1; i < 2 * size; i++ )
+	Console.WriteLine(printer.Line(i));
