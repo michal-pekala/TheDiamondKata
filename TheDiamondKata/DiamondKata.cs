b457bddc9921151d7411d2e0c39e9a7c3a7cd6fe
Michal Pekala <MP3X@sdc.dk>
2024-05-03 11:04:52 +0200

    [GREEN] DiamondKata_of_size_n_line_1_contains_n_minus_1_spaces

--- TheDiamondKata/DiamondKata.cs
+++ TheDiamondKata/DiamondKata.cs
@@ -1,13 +1,21 @@
-﻿
-namespace TheDiamondKata;
+﻿namespace TheDiamondKata;
 
 public class DiamondKata
 {
+	private readonly int _size;
+
 	public DiamondKata(int size = 1)
 	{
 		if (size is <= 0 or > 26)
 			throw new ArgumentException($"SIZE {size} IS NOT ALLOWED");
+
+		_size = size;
 	}
 
-	public string? Line(int nr) => nr == 1 ? "A" : null;
+	public string? Line(int nr)
+	{
+		var space = string.Join(null, Enumerable.Repeat(' ', _size - 1));
+
+		return nr == 1 ? space + "A" : null;
+	}
 }
