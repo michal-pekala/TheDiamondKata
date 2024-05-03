f6fddf4772abe6b3d0a4c53bcf0f72e0d6d9b783
Michal Pekala <MP3X@sdc.dk>
2024-05-03 10:27:49 +0200

    [RED] DiamondKata_line_1_is_single_letter

--- TheDiamondKata.UnitTests/UnitTest1.cs
+++ TheDiamondKata.UnitTests/UnitTest1.cs
@@ -1,11 +1,21 @@
-namespace TheDiamondKata.UnitTests
-{
-	public class UnitTest1
-	{
-		[Fact]
-		public void Test1()
-		{
+using FluentAssertions;
 
-		}
+namespace TheDiamondKata.UnitTests;
+
+public class UnitTest1
+{
+	[Fact]
+	public void DiamondKata_line_1_is_single_letter()
+	{
+		var sut = new DiamondKata();
+
+		var result = sut.Line(1);
+
+		result.Should().Be("A");
+	}
+
+	class DiamondKata
+	{
+		public string Line(int nr) => throw new NotImplementedException();
 	}
 }
