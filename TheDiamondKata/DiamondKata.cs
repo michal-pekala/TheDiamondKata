de60c86ec2041096a7221f0f0e3a3668a08ede3e
Michal Pekala <MP3X@sdc.dk>
2024-05-03 10:51:49 +0200

    [GREEN] DiamondKata size > 26 or == 0 is not allowed

--- TheDiamondKata/DiamondKata.cs
+++ TheDiamondKata/DiamondKata.cs
@@ -5,6 +5,8 @@ public class DiamondKata
 {
 	public DiamondKata(int size = 1)
 	{
+		if (size is 0 or > 26)
+			throw new ArgumentException($"SIZE {size} IS NOT ALLOWED");
 	}
 
 	public string? Line(int nr) => nr == 1 ? "A" : null;
